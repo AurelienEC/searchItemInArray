eb8f2bc13d23f0c58f5d5d6952e1eefd287de613
aurelienantonio <aurelien.antonio@gmail.com>
2019-03-25 14:42:26 -0500

    Ajout d'une personnage

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ let personnage2 = {
     name : "Aurélien",
     moves : 2,
 }
-
-const persos = [personnage1, personnage2];
+let personnage3 = {
+    name : "ennemy",
+    moves : 5,
+}
+const persos = [personnage1, personnage2, personnage3];
 
 let laurent = persos.find(elt => elt.name === 'Laurent');
 console.log("déplacements laurent", laurent.moves);
